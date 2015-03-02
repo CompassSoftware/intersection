20d1b0f3ab3d0f58656b7c8fc3883d7af48d84ae
Fennerwe <fennerwe13@gmail.com>
2015-03-01 20:27:30 -0500

    Testing git setup in Eclipse

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Test for Github repo.
+ * @author Trey Fenner
+ */
+public class Test {
+	
+	public static void main(String[] args){
+		System.out.println("This is a traffic light intersection control system!");
+	}
+}
