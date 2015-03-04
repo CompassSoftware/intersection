cfd98514252b141f5dcdbd2b167d58715e5c71ab
macdermottt <macdermottt@email.appstate.edu>
2015-03-04 13:25:14 -0500

    Tobin has connected to github

--- src/Test.java
+++ src/Test.java
@@ -8,9 +8,14 @@ public class Test {
 		System.out.println("This is a traffic light intersection control system!");
 		
 		Test test = new Test();
+		test.sayHi();
 	}
 	
 	public Test(){
 		System.out.println("I made a constructor.");
+	}
+	
+	public void sayHi(){
+		System.out.println("hi, Tobin has been successful with the git stuff");
 	}
 }
