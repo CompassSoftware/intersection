f85d36c39b800079d5e53b2ca904c07518ccb904
Fennerwe <Fennerwe@CS-ABH309-21.appd.appstate.edu>
2015-03-04 09:15:04 -0500

    Added constructor to Test

--- src/Test.java
+++ src/Test.java
@@ -7,4 +7,8 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("This is a traffic light intersection control system!");
 	}
+	
+	public Test(){
+		System.out.println("I made a constructor.");
+	}
 }
