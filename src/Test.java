8ad7147f91bb9230018608af93b01649c6ca349b
Fennerwe <Fennerwe@CS-ABH309-21.appd.appstate.edu>
2015-03-04 09:28:14 -0500

    Updated main to create new Test

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,8 @@ public class Test {
 	
 	public static void main(String[] args){
 		System.out.println("This is a traffic light intersection control system!");
+		
+		Test test = new Test();
 	}
 	
 	public Test(){
