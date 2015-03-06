d6892d3028f82014135cebd93feb12077eac2d63
haihoua <hy1509@hotmail.com>
2015-03-06 09:25:57 -0500

    Created IStdLamp

---
+++ src/IStdLamp.java
@@ -0,0 +1,12 @@
+/**
+ * interface for StdLamp
+ * @author Haihoua
+ *
+ */
+public interface IStdLamp {
+	
+	abstract public boolean red();
+	abstract public boolean yellow();
+	abstract public boolean green();
+
+}
