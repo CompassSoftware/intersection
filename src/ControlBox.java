b8040af9121d7c53047ea2f50cf07eba472c426d
Fennerwe <Fennerwe@CS-ABH309-21.appd.appstate.edu>
2015-03-06 09:46:35 -0500

    Created ControlBox class
    ControlBox is the central hub of the program that's responsible for
    controlling behavior and keeping the program running.

---
+++ src/ControlBox.java
@@ -0,0 +1,25 @@
+/**
+ * The main controller for the program.  Central hub where everything
+ * is happening.
+ * 
+ * @author Fennerwe
+ *
+ */
+public class ControlBox {
+
+	/**
+	 * No-arg constructor for creating a a ControlBox with default settings.
+	 */
+	public ControlBox(){
+		System.out.println("I am a ControlBox, hear me roar.");
+	}
+	
+	/**
+	 * Main
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args){
+		ControlBox cb = new ControlBox();
+	}
+}
