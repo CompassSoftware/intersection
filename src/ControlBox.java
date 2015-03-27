d07bde2262b1b97f3e5e12d88154214324bc93ae
BlueLeader777 <thebluejedi777@gmail.com>
2015-03-27 09:38:37 -0400

    Added defalt setings for control box

--- src/ControlBox.java
+++ src/ControlBox.java
@@ -6,12 +6,32 @@
  *
  */
 public class ControlBox {
-
+    private Timer timer;
+    //private ISensor[] sensors;
+    private IStdLamp[] lamps;
+    //private IStdCrossWalk[] crossWalks;
 	/**
 	 * No-arg constructor for creating a a ControlBox with default settings.
 	 */
 	public ControlBox(){
 		System.out.println("I am a ControlBox, hear me roar.");
+		timer = new Timer();
+		//sensors = new ISensor[4];
+		lamps = new IStdLamp[4];
+		//crossWalks = new IStdCrossWalk[8];
+		for(int i = 0; i < lamps.length; i++)
+		{ lamps[i] = new StdLamp(); }
+		//for (int i = 0; i < crossWalks.length; i++)
+		//{ crossWalks[i] = new StdCrossWalk(); }
+	}
+	
+	/**
+     * Constructor for creating a ControlBox with settings from outside.
+     */
+	public ControlBox(IStdLamp[] lamps)
+	{
+	    timer = new Timer();
+	    this.lamps = lamps;
 	}
 	
 	/**
